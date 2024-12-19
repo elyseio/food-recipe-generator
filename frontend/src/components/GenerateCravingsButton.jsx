428c71f24045a65489ebdfd23bfc97568e44051f
elyseio <elyseio@proton.me>
2024-12-19 13:22:50 +0800

    added basic code for react components

---
+++ frontend/src/components/GenerateCravingsButton.jsx
@@ -0,0 +1,8 @@
+
+function GenerateCravingsButton() {
+  return(
+    <button>generate cravings</button>
+  )
+}
+
+export default GenerateCravingsButton
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import GenerateCravingsButton from '../components/GenerateCravingsButton.jsx'
+
+function HomePage() {
+  return(
+    <div>
+      <h1>Ano crinacrave mo ngayon?</h1>
+      <GenerateCravingsButton />
+    </div>
+  )
+}
+
+export default HomePage
